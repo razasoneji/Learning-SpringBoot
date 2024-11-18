7c71b28e6c775e0cef22e8ed4ab64958812b3b07
razasoneji <razaissoneji@gmail.com>
2024-11-18 14:49:12 +0530

    Demo Project Added

---
+++ demoproject/src/test/java/com/firstproject/razasoneji/demoproject/StarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.razasoneji.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
